4311f1a86e3437e8bad00e60df044a43377e29d9
Kevin Collazos <kevcollazos@gmail.com>
2023-12-12 09:13:13 -0500

    :sparkles: feat (Theme) MOSS-14: Add theme provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'sonner';
-
-import { Providers } from '@/components';
+import { Providers, ThemeProvider } from '@/components';
 import { cn } from '@/lib/utils';
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +16,18 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={cn(inter.className, 'dark')}>
-        <Providers>{children}</Providers>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(inter.className)}>
+        <Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Providers>
         <Toaster
           toastOptions={{
             style: { background: '#18222d', color: '#fff', borderColor: '#18222d' },
