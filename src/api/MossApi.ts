c21bf961a68317686cb040a9b48dfceae7872e1a
Kevin Collazos <kevcollazos@gmail.com>
2023-12-12 09:18:44 -0500

    :recycle: refactor (API Base URL): update MossApi baseURL

--- src/api/MossApi.ts
+++ src/api/MossApi.ts
@@ -2,17 +2,7 @@
 import axios from 'axios';
 
 const MossApi = axios.create({
-  // TS
-  // baseURL: 'https://ts-uao-project-production.up.railway.app/api',
-  baseURL: 'https://moss-back-dev.up.railway.app/api',
+  baseURL: 'https://moss-back-production.up.railway.app//api',
 });
-
-// MossApi.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers['x-token'] = token;
-//   }
-//   return config;
-// });
 
 export default MossApi;
