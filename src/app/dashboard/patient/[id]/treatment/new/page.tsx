152c0e0fc6e28724bca0cb4e280127fcbbd8caa8
xKeCo <kevcollazos@gmail.com>
2023-12-28 19:23:55 -0500

    :recycle: refactor (add treatment): Refactor treatment page and add new treatment page

--- src/app/dashboard/patient/[id]/treatment/new/page.tsx
+++ src/app/dashboard/patient/[id]/treatment/new/page.tsx
@@ -1,11 +1,21 @@
+'use client';
 import { Odontogram } from '@/components';
-import React from 'react';
+import { IToothState } from '@/interfaces';
 
 export default function NewTreatmentPage() {
+  let odontogramState: IToothState[] = [];
+
   return (
     <div className="flex flex-col items-center justify-start">
-      <h1>New treatment</h1>
-      <Odontogram />
+      <h1>TreatmentPage</h1>
+      <Odontogram odontogramState={odontogramState} />
+      <button
+        onClick={() => {
+          console.log('odontogramState', odontogramState);
+        }}
+      >
+        odontogramState
+      </button>
     </div>
   );
 }
