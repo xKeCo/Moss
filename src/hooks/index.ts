13a192e76d433c92f6cc69ce7337275ec1630037
xKeCo <kevcollazos@gmail.com>
2024-01-27 14:00:55 -0500

    :recycle: refactor(Hooks) MOSS-15: Remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,1 @@
 export * from './useForm';
-export * from './useAuthStore';
-export * from './usePatientsStore';
-export * from './useTreatmentsStore';
-// export * from './useActivitiesStore';
-// export * from './useSearch';
-// export * from './useProfilesStore';
-// export * from './useSearchActivities';
-// export * from './useReportsStore';
