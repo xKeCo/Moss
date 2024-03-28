13a4548f9833923886476461d76a77992e49cb8f
xKeCo <kevcollazos@gmail.com>
2024-03-28 18:09:15 -0500

    :recycle: refactor(Confirmation Email) MOSS-25:  Updated confirmation email sending validation

--- src/actions/emails/sendConfirmationEmail.ts
+++ src/actions/emails/sendConfirmationEmail.ts
@@ -4,20 +4,23 @@ import type { IAppointment } from '@/interfaces';
 
 export const sendConfirmationEmail = async (appointment: IAppointment) => {
   try {
+    const body = JSON.stringify({
+      email: appointment?.Patient.email,
+      patientName: appointment?.Patient.name,
+      appointmentDate: appointment.date,
+      appointmentStartTime: appointment.startTime,
+      appointmentStartTimeAMPM: appointment.startTimeAMPM,
+      appointmentEndTime: appointment.endTime,
+      appointmentEndTimeAMPM: appointment.endTimeAMPM,
+    });
+
     await fetch(`${process.env.ENVIRONMENT_URL}/api/send`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(body, 'utf8').toString(),
       },
-      body: JSON.stringify({
-        email: appointment?.Patient.email,
-        patientName: appointment?.Patient.name,
-        appointmentDate: appointment.date,
-        appointmentStartTime: appointment.startTime,
-        appointmentStartTimeAMPM: appointment.startTimeAMPM,
-        appointmentEndTime: appointment.endTime,
-        appointmentEndTimeAMPM: appointment.endTimeAMPM,
-      }),
+      body,
     });
 
     await prisma.appointment.update({
