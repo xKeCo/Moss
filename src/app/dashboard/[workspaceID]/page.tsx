61eaa2ff878f44eea387ee2fc09bea81f822a064
xKeCo <kevcollazos@gmail.com>
2024-06-09 20:58:21 -0500

    :recycle: refactor(Dashboard) MOSS-29: update dashboard loading page

--- src/app/dashboard/[workspaceID]/page.tsx
+++ src/app/dashboard/[workspaceID]/page.tsx
@@ -1,4 +1,4 @@
-import { AppointmentsHomeList, AppointmentsHomeListSkeleton, PatientsList } from '@/components';
+import { AppointmentsHomeList, PatientsList } from '@/components';
 
 interface IDashboardProps {
   params: {
@@ -14,20 +14,6 @@ export default async function Dashboard({ params }: Readonly<IDashboardProps>) {
 
         <div className="flex flex-col gap-4 flex-wrap rounded-2xl p-5 border col-span-3 items-start dark:border-[#29292f] dark:bg-zinc-900">
           <PatientsList workspaceID={params.workspaceID} />
-        </div>
-      </div>
-    </main>
-  );
-}
-
-export function DashboardSkeleton() {
-  return (
-    <main className="py-5 px-4 sm:px-8">
-      <div className="grid grid-cols-1 md:grid-cols-6 xl:grid-cols-8 gap-4 h-[calc(100vh-109px)]">
-        <AppointmentsHomeListSkeleton />
-
-        <div className="flex flex-col gap-4 flex-wrap rounded-2xl p-5 border col-span-3 items-start dark:border-[#29292f] dark:bg-zinc-900">
-          <PatientsList.Skeleton />
         </div>
       </div>
     </main>
